1e4d03f256e85f958f06d4634fbf96cdc9f43dcd
Gorbenko <anton69917091@gmail.com>
2020-05-27 17:32:11 +0300

    singlePost1

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,7 @@
 <template>
-<<<<<<< HEAD
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        verdict
-      </h1>
-      <h2 class="subtitle">
-        My kickass Nuxt.js project
-      </h2>
-      <nuxt-link to="/singlePost">
-        single Post
-      </nuxt-link>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
-=======
   <div />
->>>>>>> eb0765b8044d4e8a0ab3ae027d071347fc3fe059
 </template>
 
 <script>
 export default {}
 </script>
-<<<<<<< HEAD
-
-<style scoped>
-
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-=======
->>>>>>> eb0765b8044d4e8a0ab3ae027d071347fc3fe059
