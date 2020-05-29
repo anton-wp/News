dcd7416967bddfc7fe06b90e47e41c9566df9ae3
Gorbenko <anton69917091@gmail.com>
2020-05-29 15:35:17 +0300

    search contact

---
+++ pages/contact.vue
@@ -0,0 +1,303 @@
+<template>
+  <div class="terms-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="wrapper-terms">
+            <h1 class="terms-page-title">
+              CONTACT
+            </h1>
+            <p class="text">
+              Verdict Membership Questions
+            </p>
+            <p class="text">
+              For any question related to your Verdict Member account please contact:
+            </p>
+            <p class="text">
+              Verdict Members Team -<a class="terms-text-link">members@verdict.org</a>
+            </p>
+            <br>
+            <br>
+            <div class="paragraph">
+              <p class="text">
+                Verdict Technical Problems
+              </p>
+              <p class="text">
+                For any question related to Verdict Site or App please contact:
+              </p>
+              <p class="text">
+                Verdict Support Team -
+                <a class="terms-text-link">support@verdict.org</a>
+              </p>
+            </div>
+            <br>
+            <br>
+            <div class="paragraph">
+              <p class="text">
+                Verdict Content Questions
+              </p>
+              <p class="text">
+                For any question related to Content on Verdict Site or App please contact:
+              </p>
+              <p class="text">
+                Verdict Content Team -
+                <a class="terms-text-link">content@verdict.org</a>
+              </p>
+            </div>
+            <br>
+            <br>
+            <div class="paragraph">
+              <p class="text">
+                Verdict Development
+              </p>
+              <p class="text">
+                For any question related to Verdict development including ideas:
+              </p>
+              <p class="text">
+                Verdict Community Team -
+                <a class="terms-text-link">community@verdict.org</a>
+              </p>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="paragraph">
+              <p class="text">
+                Other Questions About Verdict:
+              </p>
+              <p class="text">
+                For all other questions please fill out the form below:
+              </p>
+            </div>
+            <form class="form" [formGroup]="messageForm" (ngSubmit)="sendMail()">
+              <div class="input-unit">
+                <h3>
+                  Your Name (required)
+                </h3>
+                <input formControlName="name">
+                <div>
+                  <div class="errors">
+                    Name is required
+                  </div>
+                </div>
+              </div>
+              <div class="input-unit">
+                <h3>Your Email (required)</h3>
+                <input formControlName="email">
+                <div>
+                  <div class="errors">
+                    Email is required
+                  </div>
+                  <div class="errors">
+                    Email must be a valid email address
+                  </div>
+                </div>
+              </div>
+              <div class="input-unit">
+                <h3>Your Message</h3>
+                <textarea formControlName="message" />
+                <div>
+                  <div class="errors">
+                    Message is required
+                  </div>
+                </div>
+              </div>
+              <div class="input-unit">
+                <div class="reCaptcha">
+                  <!-- <re-captcha
+                    (resolved)="resolved($event)"
+                    formControlName="recaptchaReactive"
+                    siteKey="6Lcp8vkUAAAAAA2t0VjE6zLLHFEW1Td_OYuoTKUF"
+                  ></re-captcha> -->
+                </div>
+                <div>
+                  <div class="errors">
+                    Press the reСaptch
+                  </div>
+                </div>
+              </div>
+              <button type="submit" class="signup-btn">
+                Send mail
+              </button>
+            </form>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <follow />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Follow from '~/components/universal-components/followBlock.vue'
+
+export default {
+  components: {
+    Follow
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/utils/variables";
+@import "../assets/utils/colors";
+
+.background {
+  background-color: $secondary-bgcolor;
+}
+
+.terms-wrapper {
+  font-family: open sans, Helvetica Neue, Helvetica, Roboto, Arial, sans-serif;
+  width: 100%;
+
+  .container {
+    max-width: $global-width;
+
+    .wrapper-terms {
+      padding-left: 15px;
+    }
+
+    .terms-page-title {
+      left: -15px;
+      text-transform: uppercase;
+      font-size: 1.9em;
+      line-height: 1.5;
+      position: relative;
+      color: $dark-gray;
+      // font-family: "Open Sans";
+      font-weight: bold;
+      text-size-adjust: 100%;
+      -webkit-box-direction: normal;
+      -webkit-font-smoothing: antialiased;
+      margin: 0 15px 32px 0;
+
+      &:after {
+        content: "";
+        background-color: $primary_color;
+        height: 3px;
+        width: 1.9em;
+        position: absolute;
+        left: 0.04em;
+        bottom: 1px;
+      }
+    }
+
+    .form {
+      width: 100%;
+      padding-left: 1em;
+      padding-right: 1em;
+      margin-top: 16px;
+
+      .input-unit {
+        margin-bottom: 1em;
+
+        h3 {
+          line-height: 1.5;
+          color: #0a0a0a;
+          display: inline-block;
+          font-weight: 600;
+          font-size: 1em;
+          letter-spacing: 0.5px;
+          margin-bottom: 0.1em;
+        }
+
+        input,
+        textarea {
+          font-size: 100%;
+          line-height: 1.15;
+          margin: 0;
+          width: 100%;
+          max-width: 100%;
+          padding: 0.8em;
+
+        }
+        textarea {
+          min-height: 140px;
+        }
+
+        .errors {
+          color: $hover_color;
+        }
+
+        .reCaptcha {
+          width: 304px;
+        }
+
+        .is-invalid {
+          border: 1px solid;
+          border-color: $hover_color;
+        }
+      }
+
+      .signup-btn {
+        background-color: $primary_color;
+        color: $white;
+        cursor: pointer;
+        height: 39px;
+        padding: 0 20px;
+        font-weight: 700;
+        font-size: 0.9rem;
+        letter-spacing: 0.8px;
+        text-transform: uppercase;
+        border: none;
+        -webkit-appearance: none;
+        font-family: "Open Sans";
+        &:hover {
+          background-color: $hover_color;
+        }
+      }
+    }
+
+    .pargraph {
+
+      li {
+        margin: 50px 0;
+
+        .text {
+          margin-bottom: 0;
+        }
+      }
+
+      h5 {
+        padding-bottom: 20px;
+      }
+
+      .in-text-ul {
+        padding-left: 50px;
+      }
+    }
+
+    .text {
+      box-sizing: border-box;
+      color: rgb(10, 10, 10);
+      font-family: "Open Sans", sans-serif;
+      font-size: 18px;
+      font-weight: 400;
+      line-height: 27px;
+      margin-bottom: 0;
+      text-size-adjust: 100%;
+      -webkit-box-direction: normal;
+      -webkit-font-smoothing: antialiased;
+
+      .terms-text-link {
+        color: $primary_color;
+        text-decoration: underline;
+      }
+    }
+
+    .tab {
+      padding-left: 30px;
+    }
+  }
+}
+
+@media (max-width: 480px) {
+
+  .wrapper-terms {
+    padding-right: 0.9375rem;
+    padding-left: 0.9375rem;
+  }
+}
+</style>
---
+++ pages/search.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="policy-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 padding-0">
+          <h5 class="search-name">
+            SEARCH RESULTS FOR {{ stringSearch }}
+          </h5>
+          <!-- <h5
+            class="category-name"
+          >SEARCH RESULTS FOR {{ getSearchString() }}</h5> -->
+          <div
+            id="searchForm"
+            class="primary-form"
+          >
+            <input
+              type="search"
+              name="q"
+              class="search-filed"
+              autocomplete="off"
+              autocorrect="off"
+              min="0"
+            >
+            <svg width="20" height="20">
+              <use xlink:href="#search-icon" />
+            </svg>
+          </div>
+          <!-- <ng-container *ngIf="store.posts.length > 0">
+            <ng-container *ngFor="let post of store.posts">
+              <vrd-gvc type="full-block" [text]="true" [horizontalPost]="post"></vrd-gvc>
+            </ng-container>
+          </ng-container> -->
+        </div>
+        <!-- <div class="col-lg-8 padding-0">
+          <h4>Sorry, nothing to show</h4>
+        </div> -->
+        <div class="col-lg-4">
+          <follow />
+        </div>
+      </div>
+    </div>
+    <!-- <div *ngIf="store.pagination.next" class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="load-more-wrapper">
+            <span (click)="loadMore()">load more</span>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</template>
+
+<script>
+import Follow from '~/components/universal-components/followBlock.vue'
+
+export default {
+  components: {
+    Follow
+  },
+  data () {
+    return {
+      stringSearch: ''
+    }
+  },
+  beforeRouteUpdate (to, from, next) {
+    // console.log(to.query.q)
+    this.stringSearch = to.query.q
+    next()
+  },
+  mounted () {
+    // console.log(this.$route.query.q)
+    this.stringSearch = this.$route.query.q
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../assets/utils/variables";
+@import "../assets/utils/colors";
+
+.background {
+  background-color: $secondary-bgcolor;
+}
+
+.load-more-wrapper {
+  margin-bottom: 3em;
+  text-align: center;
+  margin-top: 3em;
+  span {
+    position: relative;
+    font-size: .95rem;
+    padding: 1.15em 2.8em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: .8px;
+    user-select: none;
+    border: 1px solid transparent;
+    border-radius: 0;
+    line-height: 1;
+    text-align: center;
+    cursor: pointer;
+    background-color: $primary_color;
+    color: $whiteFE;
+    transition: background-color .25s ease-out,color .25s ease-out;
+    &:hover {
+    background-color: $button-hover-background;
+    }
+    &:after {
+    content: "";
+    position: absolute;
+    height: 3px;
+    width: 30px;
+    left: 2.8em;
+    bottom: 0.8em;
+    background-color: rgba(255, 255, 255, 0.3);
+    }
+  }
+}
+
+.policy-wrapper {
+  margin-top: 20px;
+  width: 100%;
+
+  .container {
+    max-width: $global-width;
+
+    .padding-0 {
+      padding: 0;
+    }
+
+    .search-name {
+      text-transform: uppercase;
+      font-size: 1.9em;
+      margin: 0;
+      position: relative;
+      color: #6d6d6d;
+      font-weight: bold;
+      font-family: "Open Sans";
+      padding-left: 10px;
+      margin-bottom: 1em;
+      &::before {
+        content: '';
+        position: absolute;
+        display: block;
+        top: 35px;
+        height: 3px;
+        background-color: $primary_color;
+        content: '';
+        width: 1.9em;
+      }
+    }
+
+    .primary-form {
+      width: 100%;
+      padding-left: 10px;
+      padding-right: 10px;
+      margin-bottom: 2.9em;
+      font-size: 1.3em;
+      position: relative;
+
+      svg {
+        fill: $primary_color;
+        height: 20px;
+        width: 20px;
+        cursor: pointer;
+        top: 15px;
+        right: 20px;
+        position: absolute;
+      }
+
+      input {
+        width: 100%;
+        font-weight: 600;
+        padding: 0.4em 2em 0.4em 0.6em;
+        border: 1px solid #8d8d8d;
+        color: #0a0a0a;
+      }
+    }
+  }
+}
+
+</style>
