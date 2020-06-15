41970ae6641620d04741d147feef9f46dd6a8c35
ShershnevUA <alex.shershnev.ua@gmail.com>
2020-06-15 13:25:31 +0300

    Pm2 instances fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
 	  name      : 'verdict', // App name that shows in `pm2 ls`
 	  exec_mode : 'cluster', // enables clustering
-	  instances : 'max', // or an integer
+	  instances : '8', // or an integer
 	  // cwd       : './', // only if using a subdirectory
 	  script    : './node_modules/nuxt-start/bin/nuxt-start.js', // The magic key
   },
