e262d6d84af5ca749a08b94d9a831485c2fef2a1
Maks Fisher <adriel.black666@gmail.com>
2020-08-07 11:39:47 +0300

    fix amp

--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <component
-    v-if="data"
-    :is="type"
-    :data="data"
-    :slug="$route.params.slug"
-    :term="term"
-    :pagination="pagination"
-    :prev="prev"
-    :next="next"
-    :headerMenu="headerMenu"
-    :type="type"
-    :tag="tag"
-  />
+    <component
+        v-if="data"
+        :is="type"
+        :data="data"
+        :slug="$route.params.slug"
+        :term="term"
+        :pagination="pagination"
+        :prev="prev"
+        :next="next"
+        :headerMenu="headerMenu"
+        :type="type"
+        :tag="tag"
+    />
 </template>
 
 <script>
@@ -20,86 +20,62 @@ import Post from "@/components/layout/Post";
 import { error } from "util";
 
 export default {
-  amp: "hybrid",
-  ampLayout: "default.amp",
-  components: {
-    Post,
-    Category
-  },
-  data() {
-    return {
-      type: null,
-      data: null,
-      pagination: null,
-      term: null,
-      tag: false,
-      prev: null,
-      next: null
-    };
-  },
-  // asyncData({ $axios, $router, params }) {
-  //     const route = params.slug === "news" ? "posts/query/news" : params.slug;
+    amp: "hybrid",
+    ampLayout: "default.amp",
+    components: {
+        Post,
+        Category,
+    },
+    data() {
+        return {
+            type: null,
+            data: null,
+            pagination: null,
+            term: null,
+            tag: false,
+            prev: null,
+            next: null,
+        };
+    },
 
-  //     return $axios
-  //         .$get(`/api/${route}?limit=12`)
-  //         .then(response => {
-  //             const tag = response.type === "feed";
-  //             const type =
-  //                 response.type === "feed" ? "category" : response.type;
-  //             const data = response.data;
-  //             let term = response.term || { name: "news" };
+    async asyncData({ $axios, $router, params, redirect }) {
+        try {
+            const route =
+                params.slug === "news" ? "posts/query/news" : params.slug;
 
-  //             if (type === "post") {
-  //                 term = null;
-  //             }
-  //             const pagination = response.pagination;
+            const dataCat = await $axios.$get(`/api/${route}?limit=12`);
 
-  //             const prev = response.previous;
-  //             const next = response.next;
+            const tag = dataCat.type === "category";
+            const type = dataCat.type === "feed" ? "category" : dataCat.type;
+            const data = dataCat.data;
+            let term = dataCat.term || { name: "news" };
 
-  //             return { tag, type, data, term, pagination, prev, next };
-  //         })
-  //         .catch(error => {
-  //             // $router.push('/');
-  //         });
-  // },
+            if (type === "post") {
+                term = null;
+            }
+            const pagination = dataCat.pagination;
 
-  async asyncData({ $axios, $router, params, redirect }) {
-    try {
-      const route = params.slug === "news" ? "posts/query/news" : params.slug;
+            const prev = dataCat.previous;
+            const next = dataCat.next;
 
-      const dataCat = await $axios.$get(`/api/${route}?limit=12`);
+            const headerMenu = await $axios.$get(`/api/menu/header`);
 
-      const tag = dataCat.type === "category";
-      const type = dataCat.type === "feed" ? "category" : dataCat.type;
-      const data = dataCat.data;
-      let term = dataCat.term || { name: "news" };
+            // console.log(data);
+            return {
+                tag: tag,
+                type: type,
+                data: data,
+                term: term,
+                pagination: pagination,
+                prev: prev,
+                next: next,
 
-      if (type === "post") {
-        term = null;
-      }
-      const pagination = dataCat.pagination;
-
-      const prev = dataCat.previous;
-      const next = dataCat.next;
-
-      const headerMenu = await $axios.$get(`/api/menu/header`);
-
-      return {
-        tag: tag,
-        type: type,
-        data: data,
-        term: term,
-        pagination: pagination,
-        prev: prev,
-        next: next,
-
-        headerMenu: headerMenu.data
-      };
-    } catch (e) {
-			redirect('/404');
-    }
-  }
+                headerMenu: headerMenu.data,
+            };
+        } catch (e) {
+            redirect("/404");
+        }
+    },
 };
 </script>
 
